df6f36862fcc9949aef5fbc6846dc80edd35deca
bernie-cm <bernie.camejo@icloud.com>
2025-08-11 08:44:35 +1000

    Wrote starter ingest function

--- src/ingest.py
+++ src/ingest.py
@@ -2,3 +2,18 @@ import pandas as pd
 import logging
 import numpy as np
 
+# TODO write a function that reads a CSV and log the row count
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+def read_csv_log_rows(filename):
+    logger.info(f"Preparing to read {filename}...")
+    df = pd.read_csv(filename)
+    logger.info(f"Processed and read {len(df)} rows.")
+
+    return df
+
+# Testing
+    
+
